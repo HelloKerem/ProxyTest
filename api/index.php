0e330f2669d7fa13c2f21da4f96c0cbd339d3cac
The Generator Cell <98821815+HelloKerem@users.noreply.github.com>
2025-10-28 12:11:39 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,8 +5,8 @@ header('Expires: 0');
 $assetId = isset($_GET['assetId']) ? $_GET['assetId'] : 0;
 
 if (!isset($_GET['assetId']) || !preg_match('/^[a-zA-Z0-9_-]+$/', $_GET['assetId'])) {
-    echo file_get_contents("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}")
-    exit
+    echo file_get_contents("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}");
+    exit;
 }
 
 // Use fopen() on the remote file
