3f23fd187011eaff3833300e8605ce1762e4cc9e
The Generator Cell <98821815+HelloKerem@users.noreply.github.com>
2025-10-28 12:16:00 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,17 +1,8 @@
 <?php
-header('Pragma: no-cache');
-header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-header('Expires: 0');
-$assetId = isset($_GET['assetId']) ? $_GET['assetId'] : 0;
 header("Content-Type: application/octet-stream");
+$assetId = isset($_GET['assetId']) ? $_GET['assetId'] : 1;
 if (!isset($_GET['assetId']) || !preg_match('/^[a-zA-Z0-9_-]+$/', $_GET['assetId'])) {
-    $handle = @fopen("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}", 'rb');
-    while (!feof($handle)) {
-        echo fread($handle, 8192);
-        flush();
-    }
-    fclose($handle);
-    exit;
+    die(@file_get_contents("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}"))
 }
 $handle = @fopen("https://gitgud.io/nina11/my-guegue-project/-/raw/master/Assets/{$assetId}", 'rb');
 while (!feof($handle)) {
