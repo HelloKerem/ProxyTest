8754acc048a9a48ce2005896932a51bacd167b77
The Generator Cell <98821815+HelloKerem@users.noreply.github.com>
2025-10-28 12:33:45 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -9,14 +9,6 @@ if($assetId === 0){
 http_response_code(404);
 die("No assetId supplied."); // dont need to make errors pretty
 }
-$stmt1 = $conn->prepare("SELECT * FROM assets WHERE id = :assetId");
-$stmt1->bindParam(':assetId', $assetId);
-$stmt1->execute();
-$AssetOBJ = $stmt1->fetch(PDO::FETCH_ASSOC);
-if(!$AssetOBJ){
-http_response_code(404);
-die("Asset does not exist."); // dont need to make errors pretty
-}
 header("Content-Type: application/octet-stream");
 $ok = @file_get_contents("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}");
 if ($ok === false) {
