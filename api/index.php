bc37348e00938555dc50f30f1e4d3d5f71104263
The Generator Cell <98821815+HelloKerem@users.noreply.github.com>
2025-10-28 12:37:06 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,14 +6,17 @@ die("No assetId supplied."); // dont need to make errors pretty
 }
 $assetId = (int)$_GET['id'];
 if($assetId === 0){
-http_response_code(404);
-die("No assetId supplied."); // dont need to make errors pretty
+$someserver = @file_get_contents("https://gitgud.io/nina11/my-guegue-project/-/raw/master/Assets/{$assetId}");
+if ($someserver === false) {
+    http_response_code(404);
+    die("No assetId supplied."); // dont need to make errors pretty
+} {
+    die($someserver)
+}
 }
 header("Content-Type: application/octet-stream");
 $ok = @file_get_contents("http://rblprox.servehttp.com:81/fetchasset.php?assetId={$assetId}");
-if ($ok === false) {
-    die(@file_get_contents("https://gitgud.io/nina11/my-guegue-project/-/raw/master/Assets/{$assetId}"));
-} {
+if (!$ok === true) {
     die($ok);
 }
 ?>
