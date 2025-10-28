3a79696acb091c6c06c520dd552c45d047c66b3c
The Generator Cell <98821815+HelloKerem@users.noreply.github.com>
2025-10-28 13:00:19 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,11 +6,6 @@ if(!isset($_GET['id'])){
 }
 $assetId = (int)$_GET['id'];
 if($assetId === 0){
-    $someserver = @file_get_contents("https://gitgud.io/nina11/my-guegue-project/-/raw/master/Assets/{$_GET['id']}");
-    if ($someserver === false) {
-        http_response_code(404);
-        die("No assetId supplied.");
-    }
     $something = @file_get_contents("https://file.garden/aNTqSg4ZkRNIiewL/Assets/{$_GET['id']}");
     if ($something === false) {
         http_response_code(404);
